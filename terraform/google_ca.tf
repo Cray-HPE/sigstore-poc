5be88b074ae3fb72ad59d835b4f782f2fddd770f
James Strong <strong.james.e@gmail.com>
2022-02-09 11:17:34 -0500

    add the ca

--- terraform/google_ca.tf
+++ terraform/google_ca.tf
@@ -7,11 +7,6 @@ resource "google_privateca_ca_pool" "default" {
     publish_crl     = false
   }
   issuance_policy {
-    allowed_key_types {
-      elliptic_curve {
-        signature_algorithm = "ECDSA_P384"
-      }
-    }
     maximum_lifetime            = "50000s"
     allowed_issuance_modes {
 
@@ -22,13 +17,58 @@ resource "google_privateca_ca_pool" "default" {
 
 }
 
+resource "google_privateca_certificate_authority" "default" {
+  pool = google_privateca_ca_pool.default.name
+  certificate_authority_id = "sigstore-poc-${var.workspace_id}"
+  location = var.DEFAULT_LOCATION
+  project = var.PROJECT_ID
+
+  key_spec {
+    algorithm = "EC_P384_SHA384"
+  }
+
+  config  {
+    subject_config  {
+      subject {
+        organization = "Example, Org."
+        common_name  = "Example Authority"
+      }
+    }
+    x509_config {
+      ca_options {
+        # is_ca *MUST* be true for certificate authorities
+        is_ca = true
+        max_issuer_path_length = 10
+      }
+      key_usage {
+        base_key_usage {
+          # cert_sign and crl_sign *MUST* be true for certificate authorities
+          cert_sign = true
+          crl_sign = true
+        }
+        extended_key_usage {
+          server_auth = true
+          code_signing = true
+        }
+      }
+    }
+  }
+
+  depends_on = [
+    google_privateca_ca_pool.default
+  ]
+}
+
+output "ca_certificate" {
+  value = google_privateca_certificate_authority.default.pem_ca_certificates
+}
+
 resource "google_service_account_iam_member" "createcerts-account-iam" {
   role               = "roles/iam.workloadIdentityUser"
   member             = "serviceAccount:${var.PROJECT_ID}.svc.id.goog[fulcio-system/createcerts]"
   service_account_id = google_service_account.gke-workload.name
   depends_on         = [google_service_account.gke-workload]
 }
-
 
 resource "google_service_account_iam_member" "fulcio-account-iam" {
   role               = "roles/iam.workloadIdentityUser"
